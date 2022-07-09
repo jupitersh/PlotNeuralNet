8610931d473b926a5887cabf7b5fcd2f82af98a2
Pepper <suda0huan9@gmail.com>
2022-07-09 04:41:25 +0000

    create layers for my own

---
+++ pycore/module.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python3
+
+
+import os
+
+
+def to_head( projectpath ):
+    pathlayers = os.path.join( projectpath, 'layers/' ).replace('\\', '/')
+    return r"""
+\documentclass[border=8pt, multi, tikz]{standalone} 
+\usepackage{import}
+\subimport{"""+ pathlayers + r"""}{init}
+\usetikzlibrary{positioning}
+\usetikzlibrary{3d} %for including external image 
+"""
+
+
+def to_cor():
+    return r"""
+\def\ConvColor{rgb:red,0;green,1;blue,1}  % Aqua #00FFFF
+\def\BNColor{rgb:red,0.19;green,0.55;blue,0.91}  % Bleu de France #318CE7
+\def\ReluColor{rgb:red,0.99;green,0.93;blue,0}  % Aureolin #FDEE00
+"""
+
+
+def to_begin():
+    return r"""
+\newcommand{\copymidarrow}{\tikz \draw[-Stealth,line width=0.8mm,draw={rgb:blue,4;red,1;green,1;black,3}] (-0.3,0) -- ++(0.3,0);}
+
+\begin{document}
+\begin{tikzpicture}
+\tikzstyle{connection}=[ultra thick,every node/.style={sloped,allow upside down},draw=\edgecolor,opacity=0.7]
+\tikzstyle{copyconnection}=[ultra thick,every node/.style={sloped,allow upside down},draw={rgb:blue,4;red,1;green,1;black,3},opacity=0.7]
+"""
+
+
+def to_input( pathfile, to='(-3,0,0)', width=8, height=8, name="temp" ):
+    return r"""
+\node[canvas is zy plane at x=0] (""" + name + """) at """+ to +""" {\includegraphics[width="""+ str(width)+"cm"+""",height="""+ str(height)+"cm"+"""]{"""+ pathfile +"""}};
+"""
+
+
+def to_Conv( name, s_filer=256, n_filer=64, offset="(0,0,0)", to="(0,0,0)", width=1, height=40, depth=40, caption=" " ):
+    return r"""
+\pic[shift={"""+ offset +"""}] at """+ to +""" 
+    {Box={
+        name=""" + name +""",
+        caption="""+ caption +r""",
+        xlabel={{"""+ str(n_filer) +""", }},
+        zlabel="""+ str(s_filer) +""",
+        fill=\ConvColor,
+        height="""+ str(height) +""",
+        width="""+ str(width) +""",
+        depth="""+ str(depth) +"""
+        }
+    };
+"""
+
+
+def to_BatchNorm(name, offset="(0,0,0)", to="(0,0,0)", width=1, height=32, depth=32, opacity=0.5, caption=" "):
+    return r"""
+\pic[shift={ """+ offset +""" }] at """+ to +""" 
+    {Box={
+        name="""+name+""",
+        caption="""+ caption +r""",
+        fill=\BNColor,
+        opacity="""+ str(opacity) +""",
+        height="""+ str(height) +""",
+        width="""+ str(width) +""",
+        depth="""+ str(depth) +"""
+        }
+    };
+"""
+
+
+def to_Relu(name, offset="(0,0,0)", to="(0,0,0)", width=1, height=32, depth=32, opacity=0.5, caption=" "):
+    return r"""
+\pic[shift={ """+ offset +""" }] at """+ to +""" 
+    {Box={
+        name="""+name+""",
+        caption="""+ caption +r""",
+        fill=\ReluColor,
+        opacity="""+ str(opacity) +""",
+        height="""+ str(height) +""",
+        width="""+ str(width) +""",
+        depth="""+ str(depth) +"""
+        }
+    };
+"""
+
+
+def to_connection(of, to):
+    return r"""
+\draw [connection]  ("""+of+"""-east)    -- node {\midarrow} ("""+to+"""-west);
+"""
+
+
+def to_skip(of, to, pos=1.25):
+    return r"""
+\path ("""+ of +"""-southeast) -- ("""+ of +"""-northeast) coordinate[pos="""+ str(pos) +"""] ("""+ of +"""-top) ;
+\path ("""+ to +"""-south)  -- ("""+ to +"""-north)  coordinate[pos="""+ str(pos) +"""] ("""+ to +"""-top) ;
+\draw [copyconnection]  ("""+of+"""-northeast)  
+-- node {\copymidarrow}("""+of+"""-top)
+-- node {\copymidarrow}("""+to+"""-top)
+-- node {\copymidarrow} ("""+to+"""-north);
+"""
+
+
+def to_end():
+    return r"""
+\end{tikzpicture}
+\end{document}
+"""
+
+
+def to_generate( arch, pathname="file.tex" ):
+    with open(pathname, "w") as f: 
+        for c in arch:
+            print(c)
+            f.write(c)
