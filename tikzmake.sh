2082fb8e1d6600b5933cb8cbce5c6d7606c10e0f
Pepper <suda0huan9@gmail.com>
2022-07-09 04:41:05 +0000

    modify tikzmake

--- tikzmake.sh
+++ tikzmake.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-python $1.py 
+python3 $1.py
 pdflatex $1.tex
 
 rm *.aux *.log *.vscodeLog
-rm *.tex
+# rm *.tex
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    open $1.pdf
-else
-    xdg-open $1.pdf
-fi
+# if [[ "$OSTYPE" == "darwin"* ]]; then
+#     open $1.pdf
+# else
+#     xdg-open $1.pdf
+# fi
